4a3ad0daf8d0fc3a0ff302432b8e753af9bc3704
GissellPenado <gpenadom@uamv.edu.ni>
2025-05-14 22:27:43 -0600

    Inicialicé las variables 'secciones', 'dias' y 'estudiantes_por_seccion', y creé un diccionario para almacenar la asistencia

--- giselle1.py
+++ giselle1.py
@@ -2,3 +2,9 @@
 # Desarrolle un programa que registre la asistencia de estudiantes en tres secciones distintas de la UAM, durante cinco días de clases. 
 # Cada día se tomará asistencia a seis estudiantes por sección. 
 # El programa deberá contabilizar y mostrar el total de asistencias registradas por sección, así como el total general de la semana.
+
+secciones = ['A', 'B', 'C']
+dias=5
+estudiantes_por_seccion=6
+
+asistencias = {seccion:0 for seccion in secciones}
