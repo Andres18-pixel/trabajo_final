7d49850340424412ba7a15fa7aef486961dfc394
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-05-15 18:39:17 -0600

    agregue mi primera parte

--- carlos2.py
+++ carlos2.py
@@ -1,1 +1,12 @@
-print("Hola, soy Carlos")
+#Ejercicio 2: Uso de computadoras en laboratorios
+#Implemente un programa que simule el estado de uso de computadoras en dos laboratorios del campus. 
+# #Cada laboratorio contiene cinco filas de cuatro computadoras. 
+# #Por cada computadora se debe registrar si está ocupada o libre (puede ingresarse manualmente o simularse con valores aleatorios). 
+# Al finalizar, el programa debe mostrar el resumen de computadoras ocupadas y libres por laboratorio.
+
+# Crear dos matrices para los laboratorios, llenas con ceros inicialmente
+laboratorio1 = [[0, 0, 0, 0] for _ in range(5)]
+laboratorio2 = [[0, 0, 0, 0] for _ in range(5)]
+
+
+
