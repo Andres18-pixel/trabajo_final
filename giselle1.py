0b7fa19f06042aa881569c9fa9de35adb2684437
Andres18-pixel <ericksonlopez@uamv.edu.ni>
2025-05-15 23:02:40 -0600

    Agregue este ejercicio

--- giselle1.py
+++ giselle1.py
@@ -10,4 +10,24 @@ estudiantes_por_seccion=6
 asistencias = {seccion:0 for seccion in secciones}
 
 for dia in range (1,dias+1):
+
     print(f"\nDía {dia}: ")
+    for seccion in secciones:
+        print(f"Asistencia para sección {seccion}: ")
+        
+        asistencia_dia=0
+        for estudiante in range(1, estudiantes_por_seccion + 1):
+            asistencia=input(f"Estudiante {estudiante} estuvo presente? (s/n): ")
+            if asistencia.lower()=='s':
+                asistencia_dia +=1
+                
+        asistencias[seccion] +=asistencia_dia
+        print(f"Total de asistencias en sección {seccion} para el día {dia}: {asistencia_dia}")
+        
+print("\nResumen de asistencias:")
+total_general=0
+for seccion, total in asistencias.items():
+    print(f"Sección {seccion}: {total} asistencias")
+    total_general += total
+    
+print(f"Total general de asistencias de la semana: {total_general}")
